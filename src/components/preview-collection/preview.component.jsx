13ec1a3b185a37c26ca5666ebdaaa7f6624c05da
Jukka Edvardsson <1426249+ejukka@users.noreply.github.com>
2019-08-14 09:03:06 +0300

    Updated shop data with new ids, updated cart.utils to group items

--- src/components/preview-collection/preview.component.jsx
+++ src/components/preview-collection/preview.component.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import "./preview-styles.scss";
 
-import CollectionItem from "../collection-item/collection.component";
+import CollectionItem from "../collection-item/collection-item.component";
 
 const CollectionPreview = ({ title, items }) => (
   <div className="collection-preview">
@@ -10,8 +10,8 @@ const CollectionPreview = ({ title, items }) => (
     <div className="preview">
       {items
         .filter((item, idx) => idx < 4)
-        .map(({ id, ...otherItemProps }) => (
-          <CollectionItem key={id} {...otherItemProps} />
+        .map(item => (
+          <CollectionItem key={item.id} item={item} />
         ))}
     </div>
   </div>
